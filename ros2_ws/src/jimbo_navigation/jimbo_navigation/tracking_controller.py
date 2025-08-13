e104d1f20eb32ba711e7bf8f5a572fddaef77c58
alhedai1 <ahmedalhidai@outlook.com>
2025-08-13 10:01:13 +0900

    update

--- ros2_ws/src/jimbo_navigation/jimbo_navigation/tracking_controller.py
+++ ros2_ws/src/jimbo_navigation/jimbo_navigation/tracking_controller.py
@@ -60,9 +60,9 @@ class TrackingController(Node):
             self.tracking = True
             self.goal_pub.publish(goal_transformed)
             # self.get_logger().info(f"Sent initial goal to nav2")
-        elif self.tracking:
-            self.goal_update_pub.publish(goal_transformed)
-            # self.get_logger().info(f"Sent updated goal to nav2")
+        # elif self.tracking:
+        #     self.goal_update_pub.publish(goal_transformed)
+        #     # self.get_logger().info(f"Sent updated goal to nav2")
 
 def main(args=None):
     rclpy.init(args=args)
